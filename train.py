e4107c8d3cc83a2ba1545d6aa60f5dd275f07ade
Eric Chen <eric.chen@backflip.ai>
2024-01-14 03:18:16 +0000

    ready to train

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = PointNet(k=10)
     train_set = Model10NetDataset(dataset_path, train=True, data_augmentation=True)
-    train_set = Subset(train_set, range(10))
+    # train_set = Subset(train_set, range(10))
     test_set = Model10NetDataset(dataset_path, train=False, data_augmentation=True)
 
     # initialize trainer
